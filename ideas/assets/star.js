f16f2a13207430324760bc695a58129c93a85be7
Callum Macrae <callum@lynxphp.com>
2012-07-08 09:34:31 +0100

    Improved JS in star.js

--- ideas/assets/star.js
+++ ideas/assets/star.js
@@ -1,17 +1,19 @@
 $('.rating').each(function() {
+	"use strict";
+
 	var $this = $(this),
 		url = $this.data('vote'),
-		width = parseFloat($this.text()) * 25;
+		width = $this.text() * 25;
 
 	$this.html('<ul class="star-rating' + (url ? ' active' : '') + '"><li class="current-rating" style="width: ' + width + 'px"></li><li><a class="one-star">1</a></li><li><a class="two-stars">2</a></li><li><a class="three-stars">3</a></li><li><a class="four-stars">4</a></li><li><a class="five-stars">5</a></li></ul>');
-	$this.find('a').click(function(e) {
-		if (url)
-		{
+	$this.find('a').click(function (e) {
+		e.preventDefault();
+
+		if (url) {
 			var vote = $(this).text();
-			$.get(url + '&v=' + vote, function(message) {
-				alert(message); // phpBB 3.1 please! :O
+			$.get(url, {v: vote}, function (message) {
+				alert(message);
 			});
 		}
-		e.preventDefault();
 	});
 });
