1a6276a37947e4ab9cd979b775d5576fd2bcb1d5
Matt Friedman <maf675@gmail.com>
2015-10-17 10:23:03 -0700

    Update docblocks

--- language/en/common.php
+++ language/en/common.php
@@ -1,14 +1,10 @@
 <?php
 /**
  *
- * This file is part of the phpBB Forum Software package.
+ * Ideas extension for the phpBB Forum Software package.
  *
- * @author Callum Macrae (callumacrae) <callum@lynxphp.com>
  * @copyright (c) phpBB Limited <https://www.phpbb.com>
  * @license GNU General Public License, version 2 (GPL-2.0)
- *
- * For full copyright and license information, please see
- * the docs/CREDITS.txt file.
  *
  */
 
