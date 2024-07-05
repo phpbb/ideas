a6a89f379b92f607189642cdd0418ce43c7cbe0a
Matt Friedman <maf675@gmail.com>
2024-07-05 16:15:38 -0700

    Update phpBB requirements
    Signed-off-by: Matt Friedman <maf675@gmail.com>

--- ext.php
+++ ext.php
@@ -67,7 +67,7 @@ class ext extends \phpbb\extension\base
 	 */
 	public function is_enableable()
 	{
-		if (PHP_VERSION_ID < 70100 || phpbb_version_compare(PHPBB_VERSION, '3.3.0', '<'))
+		if (PHP_VERSION_ID < 70100 || phpbb_version_compare(PHPBB_VERSION, '3.3.0', '<') || phpbb_version_compare(PHPBB_VERSION, '4.0.0-dev', '>='))
 		{
 			return false;
 		}
