c67c66e3a3e4b06c9cb0594eb9ccd1eba0af28b4
Matt Friedman <maf675@gmail.com>
2025-07-15 16:06:11 -0700

    Fix all errors in test file

--- tests/template/status_icon_test.php
+++ tests/template/status_icon_test.php
@@ -10,30 +10,51 @@
 
 namespace phpbb\ideas\tests\template;
 
+use phpbb\config\config;
+use phpbb\event\dispatcher;
+use phpbb\filesystem\filesystem;
+use phpbb\ideas\template\twig\extension\ideas_status_icon;
+use phpbb\language\language;
+use phpbb\language\language_file_loader;
+use phpbb\path_helper;
+use phpbb\symfony_request;
+use phpbb\template\assets_bag;
+use phpbb\template\context;
+use phpbb\template\twig\environment;
+use phpbb\template\twig\extension;
+use phpbb\template\twig\lexer;
+use phpbb\template\twig\loader;
+use phpbb\template\twig\twig;
+use phpbb\user;
+use phpbb_mock_request;
+use phpbb_template_template_test_case;
+use phpbb\datetime;
+use phpbb\request\request;
+
 require_once __DIR__ . '/../../../../../../tests/template/template_test_case.php';
 
-class status_icon_test extends \phpbb_template_template_test_case
+class status_icon_test extends phpbb_template_template_test_case
 {
 	protected $test_path = __DIR__;
 
-	protected function setup_engine(array $new_config = array())
+	public function setup_engine(array $new_config = [], string $template_path = ''): void
 	{
 		global $phpbb_root_path, $phpEx;
 
 		$defaults = $this->config_defaults();
-		$config = new \phpbb\config\config(array_merge($defaults, $new_config));
-		$lang_loader = new \phpbb\language\language_file_loader($phpbb_root_path, $phpEx);
-		$this->lang = $lang = new \phpbb\language\language($lang_loader);
-		$user = new \phpbb\user($lang, '\phpbb\datetime');
+		$config = new config(array_merge($defaults, $new_config));
+		$lang_loader = new language_file_loader($phpbb_root_path, $phpEx);
+		$this->lang = $lang = new language($lang_loader);
+		$user = new user($lang, datetime::class);
 		$this->user = $user;
 
-		$filesystem = new \phpbb\filesystem\filesystem();
+		$filesystem = new filesystem();
 
-		$path_helper = new \phpbb\path_helper(
-			new \phpbb\symfony_request(
-				new \phpbb_mock_request()
+		$path_helper = new path_helper(
+			new symfony_request(
+				new phpbb_mock_request()
 			),
-			$this->createMock('\phpbb\request\request'),
+			$this->createMock(request::class),
 			$phpbb_root_path,
 			$phpEx
 		);
@@ -41,10 +62,10 @@ class status_icon_test extends \phpbb_template_template_test_case
 		$this->template_path = $this->test_path . '/templates';
 
 		$cache_path = $phpbb_root_path . 'cache/twig';
-		$context = new \phpbb\template\context();
-		$loader = new \phpbb\template\twig\loader();
-		$assets_bag = new \phpbb\template\assets_bag();
-		$twig = new \phpbb\template\twig\environment(
+		$context = new context();
+		$loader = new loader();
+		$assets_bag = new assets_bag();
+		$twig = new environment(
 			$assets_bag,
 			$config,
 			$filesystem,
@@ -52,7 +73,7 @@ class status_icon_test extends \phpbb_template_template_test_case
 			$cache_path,
 			null,
 			$loader,
-			new \phpbb\event\dispatcher(),
+			new dispatcher(),
 			[
 				'cache'			=> false,
 				'debug'			=> false,
@@ -60,7 +81,7 @@ class status_icon_test extends \phpbb_template_template_test_case
 				'autoescape'	=> false,
 			]
 		);
-		$this->template = new \phpbb\template\twig\twig(
+		$this->template = new twig(
 			$path_helper,
 			$config,
 			$context,
@@ -68,15 +89,15 @@ class status_icon_test extends \phpbb_template_template_test_case
 			$cache_path,
 			$this->user,
 			[
-				new \phpbb\template\twig\extension($context, $twig, $this->lang),
-				new \phpbb\ideas\template\twig\extension\ideas_status_icon(),
+				new extension($context, $twig, $this->lang),
+				new ideas_status_icon(),
 			]
 		);
-		$twig->setLexer(new \phpbb\template\twig\lexer($twig));
+		$twig->setLexer(new lexer($twig));
 		$this->template->set_custom_style('tests', $this->template_path);
 	}
 
-	public function data_template_status_icons()
+	public function data_template_status_icons(): array
 	{
 		return [
 			[0, ''],
